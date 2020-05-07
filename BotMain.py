465d890ae84d8345bf0eabf388ed292165c986a4
rogerpb98 <rogerpb98@gmail.com>
2020-05-07 17:23:43 +0200

    Event per notificar Connexió/Desconnexió del bot

--- BotMain.py
+++ BotMain.py
@@ -3,6 +3,28 @@ from discord.ext import commands, tasks
 
 token = 'Njk5OTIyOTA4MTc2MzE4NTU5.Xqq8-g.C2Yvl3C6v1X19saiNDHyHJ6ZOOI'
 
+client = commands.Bot(command_prefix='!')
 
+#####EVENTS#####
+
+#Event que mostra per consola quan el bot es connecta al servidor.
+@client.event
+async def on_connect():
+    print("S'ha connectat al servidor")
+
+#Event que mostra per consola quan el bot es desconnecta del servidor.
+@client.event
+async def on_disconnect():
+    print("S'ha desconnectat al servidor")
+
+################
+
+#####COMANDES#####
+
+##################
+
+#####TASKS#####
+
+###############
 
 client.run(token)
