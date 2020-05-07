6beba75515329c4c357dc3185fb034be48ce3336
rogerpb98 <rogerpb98@gmail.com>
2020-05-07 18:27:03 +0200

    minijoc "galeta de la fortuna" implementat

--- BotMain.py
+++ BotMain.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands, tasks
 
 token = 'Njk5OTIyOTA4MTc2MzE4NTU5.Xqq8-g.C2Yvl3C6v1X19saiNDHyHJ6ZOOI'
@@ -31,9 +32,27 @@ async def on_member_join(member):
 @client.event
 async def on_member_remove(member):
     print("L'usuari ",member," acaba de sortir del servidor.")
+
 ################
 
 #####COMANDES#####
+
+#comanda de prova per veure com enviar missatges per un canal de texte a Discord.
+@client.command()
+async def patata(ctx):
+    await ctx.send('patata')
+
+#Galeta de la fortuna, utilitzant la llibrería "random" el bot escollirà una frase de l'array "frases" aleatoriament i la mostrarà per discord.
+@client.command()
+async def fortuna(ctx):
+    frases=[
+        'Tendrás un día de alegrías y buenos momentos, disfrútalos como nunca.',
+        'Concéntrate en lo que quieres lograr y ganaras. No lo olvides.',
+        'El cielo sera tu limite, pues grandes acontecimientos te sucederán.',
+        'Te sentirás feliz como un niño y veras al mundo con sus ojos.',
+        'Vivirás tu vejez con comodidades y riquezas materiales.'
+    ]
+    await ctx.send(random.choice(frases))
 
 ##################
 
